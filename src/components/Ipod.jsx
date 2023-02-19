e2a3392e1eb08afb8cb70296f8b353872db9bfc8
Krishan Kumar <kkrishan7074@gmail.com>
2023-02-19 16:44:38 +0530

    Commit

--- src/components/Ipod.jsx
+++ src/components/Ipod.jsx
@@ -9,18 +9,19 @@ import {
 
 function Ipod() {
   return (
-    <div id="wheel-contianer">
+    <div id="wheel-container">
       <div id="wheel">
-        <div className="wheel-icon">
+        <div className="wheel-icons">
           <p>Menu</p>
         </div>
-        <div className="wheel-icon">
+        <div className="wheel-icons" id="forward-icon">
           <FontAwesomeIcon icon={faFastForward} />
         </div>
-        <div className="wheel-icon">
+        <span id="menu-selector"></span>
+        <div className="wheel-icons" id="backward-icon">
           <FontAwesomeIcon icon={faFastBackward} />
         </div>
-        <div className="wheel-icon">
+        <div className="wheel-icons" id="play-pause-icon">
           <FontAwesomeIcon icon={faPlay} />
           <FontAwesomeIcon icon={faPause} />
         </div>
